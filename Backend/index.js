67c0486a98511f11865227aa4bd8a71ba7338da1
Kamran Khot <kamran29khot@gmail.com>
2024-01-04 21:05:50 +0530

    changes in cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,13 @@ const mongoose = require("mongoose");
 const login = require("./controllers/loginController");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://lms-1-rouge.vercel.app/"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connecting database
